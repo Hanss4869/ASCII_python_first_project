29f95ee3b552ba04d49d42e2bee799e95e13d2f7
hana <nurulhanamuslih@gmail.com>
2022-09-09 12:55:23 +0800

    first commit

---
+++ pythonFirst.py
@@ -0,0 +1,14 @@
+name = '''
+H       H     A     N       N      A
+H       H   A   A   N N     N    A   A
+H       H A       A N  N    N  A       A
+H H H H H A A A A A N   N   N  A A A A A
+H       H A       A N    N  N  A       A
+H       H A       A N     N N  A       A
+H       H A       A N       N  A       A
+'''
+
+print(name)
+
+#my first python project with ASCII art
+#here is my shorthand initial name
